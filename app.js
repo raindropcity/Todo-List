6f5c95ad5457adc6b225bab7e6d55df7527bfa4a
raindropcity <raindropcity0209@gmail.com>
2021-12-05 21:37:59 +0800

    add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+// 取得資料庫連線狀態
+const db = mongoose.connection
 const port = 3000
+
+mongoose.connect('mongodb://localhost/todo_list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+// 資料庫連線異常
+// db.on()：在這裡用 on 註冊一個事件監聽器，用來監聽 error 事件有沒有發生。
+db.on('error', () => {
+  console.log('mongodb error')
+})
+
+// 資料庫連線成功
+// db.once()：針對「連線成功」的 open 情況，註冊一個事件監聽器，相對於「錯誤」，連線成功只會發生一次，所以這裡特地使用 once，一旦連線成功，在執行 callback 以後就會解除監聽器。
+db.once('open', () => {
+  console.log('mongodb connected')
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World')
