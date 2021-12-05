4b5016f5ff63ef672edf425d682c5b6f7e97a0c7
raindropcity <raindropcity0209@gmail.com>
2021-12-05 22:09:18 +0800

    add todo seeder

--- models/todo.js
+++ models/todo.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+// 把想要的資料結構當成參數傳給 new Schema()
 const todoSchema = new Schema({
   name: {
-    type = String,
-    required = true
+    type: String,
+    required: true
   }
 })
 
+// 匯出的時候把這份 schema 命名為 Todo，以後在其他的檔案直接使用 Todo 就可以操作和「待辦事項」有關的資料了
 module.exports = mongoose.model('Todo', todoSchema)
