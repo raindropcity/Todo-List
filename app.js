49ef86a12297ee6951026900f1e4bf4662c59b87
raindropcity <raindropcity0209@gmail.com>
2021-12-05 22:53:52 +0800

    modify index.hbs

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 // 取得資料庫連線狀態
 const db = mongoose.connection
 const port = 3000
-const exphbs = require('express-handlebars')
+
+const Todo = require('./models/todo')
 
 mongoose.connect('mongodb://localhost/todo_list', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -20,11 +22,20 @@ db.once('open', () => {
   console.log('mongodb connected')
 })
 
+// 建立一個名為hbs的樣板引擎，並傳入exphbs的相關參數
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+// 啟用樣板引擎hbs
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  // find()：取出 Todo model 裡的所有資料，現在沒有傳入任何參數，所以會撈出整份資料。
+  // lean()：把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列，這裡可以記一個口訣：「撈資料以後想用 res.render()，要先用 .lean() 來處理」。
+  // .then() 這一步資料會被放進 todos 變數
+  // catch()：如果有錯誤的話先把錯誤內容印出來
+  Todo.find().lean().then((todos) => {
+    res.render('index', { todos: todos })
+  })
+    .catch((error) => { console.error(error) })
 })
 
 app.listen(port, () => {
